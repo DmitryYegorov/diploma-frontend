959dbe581ce52084750a5e172bfbe7c667256c41
Dmitry Yegorov <dmitry.yegorov@effective-soft.com>
2022-04-17 03:00:03 +0300

    added logout button

--- src/components/UserAuthPanel/index.tsx
+++ src/components/UserAuthPanel/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, IconButton, Stack, Typography } from "@mui/material";
+import { Logout as LogoutIcon } from "@mui/icons-material";
 import { useAppSelector } from "../../hooks/redux";
 import { useStyles } from "./styled";
+import { useNavigate } from "react-router-dom";
 
 const UserAuthPanel: React.FC = () => {
   const {
@@ -10,13 +12,25 @@ const UserAuthPanel: React.FC = () => {
   const classes = useStyles();
 
   const userName = `${user.firstName} ${user.middleName} ${user.lastName}`;
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    localStorage.removeItem("userData");
+
+    navigate("/auth/login");
+  };
 
   return (
-    <Box>
+    <Stack direction="row" alignItems="center" spacing={0.5}>
       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
         {userName}
       </Typography>
-    </Box>
+      <IconButton onClick={logout}>
+        <LogoutIcon style={{ color: "white" }} />
+      </IconButton>
+    </Stack>
   );
 };
 
